e4bd5f85d6892a1182c9983e9947d18e1ccb9495
Vlad Buryak <vladislavburyak00@gmail.com>
2020-04-13 17:41:39 +0300

    [new] UserDTO

---
+++ src/Web/DTOs/UserDTO.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jineo.DTOs
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
