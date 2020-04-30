3ce0cda971e4369678c42a330101f62d987668a3
Vlad Buryak <vladislavburyak00@gmail.com>
2020-04-30 13:05:17 +0300

    [+] dto for sensormodel

---
+++ src/Web/DTOs/SensorDTO.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Jineo.DTOs
+{
+    public class SensorDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TypeId {get;set;}
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+}
